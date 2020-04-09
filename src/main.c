e755a8dbcb65de00b509a10a43129b5a526f1524
matt kramara <mattkramara@gmail.com>
2020-04-09 13:32:17 +1000

    Re-instate main hopefully

--- src/main.c
+++ src/main.c
@@ -1,14 +1,18 @@
 #include "model.h"
 #include "window.c"
 int main(int argc, char **argv) {
+	glfwInit();
   GraphicsState graphics = InitGraphics();
   Model model = loadFromFile("./data/SpaceShipDetailed.obj");
   uint32_t shipDef = CreateEntityDef(&graphics, &model);
-  Instance inst = {.position = {0, 0, 0}, .scale = {1, 1, 1}};
+  Instance inst = {.position = {0, 0, 0}, .scale = {0.01, 0.01, 0.01}};
 	glm_mat4_identity(inst.rotation);
   AddEntityInstance(&graphics.entities[shipDef], inst);
 
   while (true) {
+		if(glfwWindowShouldClose(graphics.window)) {
+			return 0;
+		}
     glfwPollEvents();
 		MoveCamera(&graphics);
     DrawGraphics(&graphics);
